0462fb5c3fb2f2ee887dce4677204fc9c8f9cddd
Toshiki18 <toshiki2413@icloud.com>
2024-12-11 22:50:58 +0900

    feature/1ヶ月分のダッシュボードの作成

--- src/usecase/create_datamart.py
+++ src/usecase/create_datamart.py
@@ -23,7 +23,7 @@ class CreateDatamart:
         # 加工を一括で行う。
         self.create_date()  # 日付の生成
         self.date_to_cashtrade()  # データの加工
-        self.save_trade_data() # 加工データの保存
+        self.save_trade_data()  # 加工データの保存
 
     def create_date(self) -> None:
         """
@@ -68,8 +68,7 @@ class CreateDatamart:
 
         # グリッドデータを作成する
         self.closs_joined_date = (
-            self.df_date.select(pl.col("date").alias("取引日"))
-            .join(
+            self.df_date.select(pl.col("date").alias("取引日")).join(
                 _cash_trade,
                 on=["取引日"],
                 how="left",
@@ -78,20 +77,27 @@ class CreateDatamart:
             .with_columns(pl.col("現在（貸付）高").fill_null(strategy="forward"))
         )
         self._closs_joined_date_fillnull = (
-            self.closs_joined_date
-            .with_columns([
-                pl.col("払出金額（円）").fill_null(0),
-                pl.col("受入金額（円）").fill_null(0)
-            ])
+            self.closs_joined_date.with_columns(
+                [
+                    pl.col("払出金額（円）").fill_null(0),
+                    pl.col("受入金額（円）").fill_null(0),
+                ]
+            )
             # 直前の取引履歴がない場合は、(直後の残高) + (直前の払出金額) - (直前の受入金額)
-            .with_columns([
-                pl.col("現在（貸付）高").shift(-1).alias("次_現在（貸付）高"),
-                pl.col("払出金額（円）").shift(-1).alias("次_払出金額（円）"),
-                pl.col("受入金額（円）").shift(-1).alias("次_受入金額（円）")
-            ])
+            .with_columns(
+                [
+                    pl.col("現在（貸付）高").shift(-1).alias("次_現在（貸付）高"),
+                    pl.col("払出金額（円）").shift(-1).alias("次_払出金額（円）"),
+                    pl.col("受入金額（円）").shift(-1).alias("次_受入金額（円）"),
+                ]
+            )
             .with_columns(
                 pl.when(pl.col("現在（貸付）高").is_null())
-                .then(pl.col("次_現在（貸付）高") + pl.col("次_払出金額（円）") + pl.col("次_受入金額（円）"))
+                .then(
+                    pl.col("次_現在（貸付）高")
+                    + pl.col("次_払出金額（円）")
+                    + pl.col("次_受入金額（円）")
+                )
                 .otherwise(pl.col("現在（貸付）高"))
                 .alias("残高")
             )
@@ -100,26 +106,28 @@ class CreateDatamart:
                 .then(pl.col("残高").drop_nulls().first())
                 .otherwise(pl.col("残高"))
             )
+            # 収支の計算
+            .with_columns(
+                (pl.col("受入金額（円）") - pl.col("払出金額（円）")).alias("収支")
+            )
         )
 
     # csvファイルとして保存する
     def save_trade_data(self) -> None:
         """
-            1ヶ月分の取引履歴から必要なカラムを抽出し、tmpファイルとして保存する
-            Args:
-                self._closs_joined_date_fillnull: 残高の穴埋めを行ったデータ
+        1ヶ月分の取引履歴から必要なカラムを抽出し、tmpファイルとして保存する
+        Args:
+            self._closs_joined_date_fillnull: 残高の穴埋めを行ったデータ
         """
 
         _save_path = self.config["tmp"]["file_path"]
-        self._save_data = (
-            self._closs_joined_date_fillnull
-            .select(
-                pl.col("取引日"),
-                pl.col("受入金額（円）").alias("収入"),
-                pl.col("払出金額（円）").alias("支出"),
-                pl.col("詳細１").alias("種類"),
-                pl.col("詳細２").alias("対象"),
-                pl.col("残高"),
-            )
+        self._save_data = self._closs_joined_date_fillnull.select(
+            pl.col("取引日"),
+            pl.col("受入金額（円）").alias("収入"),
+            pl.col("払出金額（円）").alias("支出"),
+            pl.col("詳細１").alias("種類"),
+            pl.col("詳細２").alias("対象"),
+            pl.col("残高"),
+            pl.col("収支"),
         )
         self.__rwc.write_csv(_save_path, self.target_ym, self._save_data)
